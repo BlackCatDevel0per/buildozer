5462abd9a0777713204423be85637905ba5705ea
Asad Sayfiyev <bcdev@mail.ru>
2024-01-07 09:05:15 +0000

    Fixes..

--- buildozer/scripts/cachetools.py
+++ buildozer/scripts/cachetools.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from pathlib import Path
 from os import environ
 
@@ -7,10 +5,10 @@ use_git_caching = environ.get('USE_GIT_CACHING')
 
 ct_path = Path(__file__).parent
 # workaround script to avoid some source changes..
-git_cache_script = Path(ct_path, 'bash', 'git_cache')
+git_cache_script = str(Path(ct_path, 'bash', 'git_cache'))
 
 
-def select_git(*, allow_cache: bool = False, force_cache: bool = False) -> Union[Path, str]:
+def select_git(*, allow_cache: bool = False, force_cache: bool = False) -> str:
 	# DOTO: rich logging..
 	# print(allow_cache, use_git_caching)
 	if allow_cache and any((use_git_caching, force_cache)):
